4ac601f031d2e92a04f29cfbe609fef1cccf4a32
sosuisen <hidekazu.kubota@gmail.com>
2023-03-22 21:55:39 +0900

    fix: first commit

---
+++ src/main/java/com/example/MyApp.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MyApp extends Application {
+
+	@Override
+	public void start(Stage stage) {
+		var tField = new TextField();
+		var tArea = new TextArea();
+		
+		var vBox = new VBox();
+		vBox.getChildren().add(tField);
+		vBox.getChildren().add(tArea);
+
+		var scene = new Scene(vBox, 320, 200);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
+
+}
