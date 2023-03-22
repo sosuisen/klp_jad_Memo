502acc243bf3a74505969a6a04265b4909509251
sosuisen <hidekazu.kubota@gmail.com>
2023-03-22 23:32:45 +0900

    feat: add editable list view

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -5,19 +5,36 @@ import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MyApp extends Application {
-	
+
 	@Override
 	public void start(Stage stage) {
 		var tField = new TextField();
 		var listView = new ListView<String>();
-		ObservableList<String> list = listView.getItems();
+		ObservableList<String> model = listView.getItems();
 		
+		// 編集できるようにする
+		listView.setEditable(true);
+		listView.setCellFactory(TextFieldListCell.forListView());
+		listView.setOnEditCommit(e -> {
+		    // 編集されたセルの値を取得
+		    String newStr = e.getNewValue();
+		    // 何番目のセルが編集されたかを取得
+		    int index = e.getIndex();
+		    // modelのindex番目の値をnewStrに変更する
+		    model.set(index, newStr);
+		    // 現在のmodelの内容を表示
+			model.forEach(str->{
+				System.out.println(str);
+			});
+		});
+	
 		tField.setOnAction(e -> {
-			list.add(tField.getText());
+			model.add(tField.getText());
 			tField.clear();
 		});
 
