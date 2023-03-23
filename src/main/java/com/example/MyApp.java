2e3d2628f29c6cfee87d9cc40f3ab3c3fbb34571
sosuisen <hidekazu.kubota@gmail.com>
2023-03-23 18:38:51 +0900

    feat: add answer

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -1,29 +1,46 @@
 package com.example;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MyApp extends Application {
-	
+	private ObservableList<Node> model;
+
+	private void addItem(String txt) {
+		model.add(new TextField(txt));
+	}
+
 	@Override
 	public void start(Stage stage) {
 		var tField = new TextField();
-		var tArea = new TextArea();
-		
+
 		tField.setOnAction(e -> {
-			tArea.appendText(tField.getText() + "\n");
+			addItem(tField.getText());
 			tField.clear();
 		});
 
-		var vBox = new VBox();
-		vBox.getChildren().add(tField);
-		vBox.getChildren().add(tArea);
+		Button btn = new Button("コンソールへ出力");
+		// ボタンを押すと、リストの内容をコンソールへ出力	
+		btn.setOnAction(e -> model.forEach(node -> System.out.println(((TextField) node).getText())));
 
-		var scene = new Scene(vBox, 320, 200);
+		// 画面レイアウト用のVBoxとリスト表示用のVBoxは分けておいたほうが便利
+		VBox layout = new VBox();
+		VBox textFieldList = new VBox();
+
+		// レイアウト
+		layout.getChildren().add(tField);
+		layout.getChildren().add(btn);
+		layout.getChildren().add(textFieldList);
+
+		model = textFieldList.getChildren();
+
+		var scene = new Scene(layout, 320, 500);
 		stage.setScene(scene);
 		stage.show();
 	}
