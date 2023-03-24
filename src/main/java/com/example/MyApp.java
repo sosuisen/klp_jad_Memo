caf4aca8623aaeb4787689f0e2e57eb72f0988af
sosuisen <hidekazu.kubota@gmail.com>
2023-03-24 15:01:20 +0900

    fix: move model to local

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -11,28 +11,15 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MyApp extends Application {
-	private ObservableList<Node> model;
 
 	@Override
 	public void start(Stage stage) {
+		ObservableList<Node> model;
+		
 		var tField = new TextField();
-
-		tField.setOnAction(e -> {
-			model.add(new TextField(tField.getText()));
-			tField.clear();
-		});
-
 		Button outputBtn = new Button("コンソールへ出力");
-		// ボタンを押すと、リストの内容をコンソールへ出力	
-		outputBtn.setOnAction(e -> model.forEach(node -> System.out.println(((TextField) node).getText())));
-
 		// 発展課題 4b
 		Button removeBtn = new Button("削除");
-		// ボタンを押すと、リストの最後尾を削除
-		removeBtn.setOnAction(e -> {
-			if (model.size() > 0)
-				model.remove(model.size() - 1);
-		});
 
 		// 画面レイアウト用のVBoxとリスト表示用のVBoxは分けておいたほうが便利
 		VBox layout = new VBox();
@@ -47,6 +34,20 @@ public class MyApp extends Application {
 		// 発展課題 4b
 		model = textFieldList.getChildren();
 		model.addListener((ListChangeListener.Change<? extends Node> c) -> System.out.println("リストが変更されました"));
+		// ボタンを押すと、リストの最後尾を削除
+		removeBtn.setOnAction(e -> {
+			if (model.size() > 0)
+				model.remove(model.size() - 1);
+		});
+
+		// ボタンを押すと、リストの内容をコンソールへ出力	
+		outputBtn.setOnAction(e -> model.forEach(node -> System.out.println(((TextField) node).getText())));
+
+		// ボタンを押すと項目を追加
+		tField.setOnAction(e -> {
+			model.add(new TextField(tField.getText()));
+			tField.clear();
+		});
 
 		var scene = new Scene(layout, 320, 500);
 		stage.setScene(scene);
